a32a265b9a833164d6749b2c2efbb5ab8e411c94
El loza <loza@usal.es>
2019-10-26 16:51:14 +0200

    first changes

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const WebSocket = require('ws');
+'use strict';
 
-var HOST = location.origin.replace(/^http/, 'ws')
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
 
-const wss = new WebSocket.Server({host:HOST, port: PORT });
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX) )
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
 
 wss.on('connection', function connection(ws) {
+  console.log('Client connected');
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
